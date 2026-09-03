dccf9b1a8d9438a4af9fc56bc18241b01d0f454a
Artur Bekauov <Artbeka@yandex.ru>
2026-09-03 23:06:40 +0300

    feat(main):add lab 6

---
+++ labs/lab05/project/scripts/dining_philosophers.jl
@@ -0,0 +1,34 @@
+# # Базовые эксперименты: Обедающие филосовы
+#
+# Сравнение классической сети Петри и сети с арбитром
+using DrWatson
+@quickactivate "project"
+include(srcdir("DiningPhilosophers.jl"))
+using .DiningPhilosophers
+using DataFrames, CSV, Plots
+
+# ### Параметры моделирования 
+N = 5
+tmax = 50.0
+
+# ### Классическая сеть (без арбитра)
+
+println("=== Классическая сеть (без арбитра) ===")
+net_classic, u0_classic, _ = build_classical_network(N)
+df_classic = simulate_stochastic(net_classic, u0_classic, tmax)
+CSV.write(datadir("dining_classic.csv"), df_classic)
+dead = detect_deadlock(df_classic, net_classic)
+println("Deadlock обнаружен: $dead")
+plot_classic = plot_marking_evolution(df_classic, N)
+savefig(plotsdir("classic_simulation.png"))
+
+# ### Сеть с арбитром
+
+println("\n=== Сеть с арбитром ===")
+net_arb, u0_arb, _ = build_arbiter_network(N)
+df_arb = simulate_stochastic(net_arb, u0_arb, tmax)
+CSV.write(datadir("dining_arbiter.csv"), df_arb)
+dead_arb = detect_deadlock(df_arb, net_arb)
+println("Deadlock обнаружен: $dead_arb")
+plot_arb = plot_marking_evolution(df_arb, N)
+savefig(plotsdir("arbiter_simulation.png"))
